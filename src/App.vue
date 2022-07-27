1e34e10855396d913130110a90250748a693aae2
madshox <63535913+madshox@users.noreply.github.com>
2022-07-27 11:53:43 +0500

    move notes project to vue-cli

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="wrapper">
+    
+      <!-- <header></header> -->
+
+      <div class="wrapper-content">
+        <section>
+          <div class="container">
+            <h1>Hello</h1>
+            <img alt="Vue logo" src="./assets/logo.png" />
+          </div>
+        </section>
+      </div>
+
+      <!-- <footer></footer> -->
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+</style>
